26c9acb7c3b01fd56e38be4f7863161cc11bee06
WalidurTanjim <mohammadtanjim91@gmail.com>
2025-04-20 01:01:17 +0600

    Loading added on the root

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+    return (
+        <section className={`loading-page w-full h-screen flex items-center justify-center bg-[#fff]`}>
+            <h1 className={`text-2xl font-medium text-slate-700`}>Loading page runing...</h1>
+        </section>
+    );
+};
+
+export default loading;
